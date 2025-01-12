c806f88c857f543cf6e717aabf4bf98264a20e29
Manjeet Singh <99804338+Manaregr8@users.noreply.github.com>
2025-01-12 10:31:23 +0000

    added links over image

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -9,7 +9,7 @@ const testimonialData: Testimonial[] = [
     designation: "Founder of Zeeloops",
     content:
       "The Zeeloops website is now responsive, fast, attractive, and SEO-optimized. The transformation has been remarkable, and the attention to detail has significantly improved our online presence. I'm thrilled with the results and the professionalism displayed throughout the project!",
-    image: "/images/brands/manik.PNG",
+    image: "/images/brands/abhishek.jpg",
     star: 5,
   },
   {
@@ -18,7 +18,7 @@ const testimonialData: Testimonial[] = [
     designation: "Founder of Respikart | Surgikart",
     content:
       "The team did an outstanding job optimizing our SEO, enhancing our website's UI/UX, and automating our Google My Business profiles. Their innovative WhatsApp marketing strategies have also boosted our customer engagement. It's been a game-changer for our business!",
-    image: "/images/brands/deepek.PNG",
+    image: "/images/brands/sachin.jpg",
     star: 5,
   },
   {
@@ -27,7 +27,7 @@ const testimonialData: Testimonial[] = [
     designation: "Founder of TerxLabs",
     content:
       "Our collaboration with this team has been invaluable. They helped us serve several clients effectively and made complex projects possible with their expertise and coordination. The partnership has been a significant factor in our collaborative success, Looking forward for more",
-    image: "/images/brands/rida.PNG",
+    image: "/images/brands/komal.jpg",
     star: 5,
   },
   
